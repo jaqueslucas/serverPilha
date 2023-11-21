783d4c78ff0fa36c3c3676f7074bdecf9a7ea740
jaqueslucas <lucassouza2347@hotmail.com>
2023-11-21 04:16:09 -0300

    AGORA SIM TA FUNCIONANDO UFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/controllers/Arquivo.ts
+++ src/controllers/Arquivo.ts
@@ -1,9 +1,12 @@
 import fs from 'fs-extra';
+import path from 'path';
+
+const dataPath = path.join(__dirname,"./dados.txt")
 
 class Arquivo {
   async lerArquivo(): Promise<string[] | never> {
     try {
-      const data = await fs.readFile('dados.txt', 'utf-8');
+      const data = await fs.readFile(dataPath, 'utf-8');
       return data.split('\n');
     } catch (error) {
       throw new Error('Erro ao ler o arquivo');
@@ -12,7 +15,7 @@ class Arquivo {
 
   async escreverArquivo(nomes: string[]): Promise<void | never> {
     try {
-      await fs.writeFile('dados.txt', nomes.join('\n'));
+      await fs.writeFile(dataPath, nomes.join('\n'));
     } catch (error) {
       throw new Error('Erro ao escrever no arquivo');
     }
